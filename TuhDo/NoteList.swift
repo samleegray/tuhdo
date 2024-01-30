6f003ec5671f85e93fda0e4f80709e426ae4d30c
Sam Gray <samleegray@gmail.com>
2024-01-30 06:17:10 -0600

    Made a specific preview set of items so we can easily see changes in Preview. Added Title to items, just need to get editing functionality in the detail view.

--- TuhDo/NoteList.swift
+++ TuhDo/NoteList.swift
@@ -9,25 +9,35 @@ import SwiftUI
 import SwiftData
 
 struct NoteList: View {
+    /// Not 100% sure, need to check this out. -Sam
     @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
     
+    /// The items we are listing.
+    @Query private var items: [Item]
+    var previewItems: [Item]?
+    
+    /// Any localizable strings for this view.
     private struct LocalizedStrings {
-        static let title = "TuhDo List"
+        static let title = NSLocalizedString("note_list_view_title", value: "TuhDo List", comment: "TuhDo List, a take on Todo List")
+        static let defaultDetailPageViewText = NSLocalizedString("default_detail_page_view_text", value: "Select an item.", comment: "Displays when nothing is selected in the details view.")
+        static let addItemLabel = NSLocalizedString("add_item_label", value: "Add Item", comment: "Add Item.")
     }
     
+    /// New item text.
     @State var newItemText: String = ""
 
     var body: some View {
-        
         NavigationSplitView {
             // List of all items
             List {
-                ForEach(items) { item in
+                ForEach(previewItems ?? items) { item in
                     NavigationLink {
                         NoteDetails(item: item)
                     } label: {
                         VStack(alignment: .leading) {
+                            if let title = item.title {
+                                Text(title).font(.headline)
+                            }
                             Text(item.text)
                             Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
                                 .font(.footnote).foregroundStyle(.secondary)
@@ -36,45 +46,65 @@ struct NoteList: View {
                     }
                 }
                 .onDelete(perform: deleteItems)
+            // Toolbar for navigation items
             }.toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     EditButton()
                 }
                 ToolbarItem {
                     Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
+                        Label(LocalizedStrings.addItemLabel, systemImage: "plus")
                     }
                 }
             }.navigationBarTitleDisplayMode(.large)
                 .navigationTitle(LocalizedStrings.title)
             
         } detail: {
-            Text("Select an item").font(.subheadline)
+            // The default detail page when we have nothing to display.
+            Text(LocalizedStrings.defaultDetailPageViewText).font(.subheadline)
         }
         
         // Bottom entry section.
         QuickEntry(newItemText: $newItemText)
     }
 
+    /// Add an item to our storage.
     private func addItem() {
         withAnimation {
-            print("newItemText: \(newItemText)")
+            // Create & insert new item.
             let newItem = Item(timestamp: Date(), text: newItemText)
             modelContext.insert(newItem)
-            newItemText = ""
+            // Reset.
+            reset()
         }
     }
 
+    /// Delete an item from our storage.
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
+            // Remove the item.
             for index in offsets {
                 modelContext.delete(items[index])
             }
         }
     }
+    
+    /// Reset UI to default after an item is created.
+    private func reset() {
+        newItemText = ""
+    }
 }
 
-#Preview {
-    NoteList()
-        .modelContainer(for: Item.self, inMemory: true)
+struct NoteList_Previews: PreviewProvider {
+    static let items = [
+        Item(timestamp: Date.distantPast, text: "An old todo.", title: "Old Title Todo"),
+        Item(timestamp: Date.distantFuture, text: "A note from the future!", title: "Future Note")
+    ]
+    
+    static var previews: NoteList = {
+        return NoteList(previewItems: items)
+    }()
+    
+    typealias Previews = NoteList
+//        .modelContainer(for: Item.self, inMemory: true)
 }
