f2e982f23988add94d4d63863bc8848715f94f99
Sam Gray <samleegray@gmail.com>
2024-09-17 20:04:13 -0500

    Got flag priority UI functional
    Got it showing the flag levels and changing it's icon accordingly.

--- TuhDo/QuickEntry.swift
+++ TuhDo/QuickEntry.swift
@@ -7,9 +7,17 @@
 
 import SwiftUI
 
+enum Priority: String, Codable, CaseIterable, Identifiable {
+    case none, low, medium, high
+    
+    var id: Self { self }
+}
+
 struct QuickEntry: View {
     
     @Binding var newItemText: String
+    @State private var showingPriorityPopover = false
+    @State private var priority: Priority = .none
     
     let newItemTextCornerRadius: CGFloat = 0
     let newItemTextMinHeight: CGFloat = 40
@@ -26,11 +34,74 @@ struct QuickEntry: View {
     func getTextView() -> some View {
         HStack {
             VStack(alignment: .leading) {
+                HStack {
+                    Spacer()
+                    Button(action: setPrirority) {
+                        switch priority {
+                        case .none:
+                            Image(systemName: "flag")
+                        case .low:
+                            Image(systemName: "flag.fill").foregroundStyle(.blue)
+                        case .medium:
+                            Image(systemName: "flag.fill").foregroundStyle(.orange)
+                        case .high:
+                            Image(systemName: "flag.fill").foregroundStyle(.red)
+                        }
+                    }
+                    .padding(.trailing, 16)
+                    .popover(isPresented: $showingPriorityPopover,
+                             attachmentAnchor: .point(.top),
+                             arrowEdge: .bottom) {
+                        VStack(alignment: .leading) {
+                            Button(action: setPriorityHigh) {
+                                HStack {
+                                    Image(systemName: "flag.fill")
+                                        .foregroundStyle(.red)
+                                    Text("High")
+                                }
+                            }
+                            Button(action: setPriorityMedium) {
+                                HStack {
+                                    Image(systemName: "flag.fill")
+                                        .foregroundStyle(.orange)
+                                    Text("Medium")
+                                }
+                            }
+                            Button(action: setPriorityLow) {
+                                HStack {
+                                    Image(systemName: "flag.fill")
+                                        .foregroundStyle(.blue)
+                                    Text("Low")
+                                }
+                            }
+                        }.padding()
+                            .presentationCompactAdaptation(.none)
+                    }
+                }
                 TextField(LocalizedStrings.placeholder, text: $newItemText, axis: .vertical)
                     .cornerRadius(newItemTextCornerRadius)
                     .frame(minHeight: newItemTextMinHeight)
                     .padding()
             }
         }
+    }
+    
+    func setPrirority() {
+        showingPriorityPopover = !showingPriorityPopover
+    }
+    
+    func setPriorityHigh() {
+        priority = .high
+        showingPriorityPopover = false
+    }
+    
+    func setPriorityMedium() {
+        priority = .medium
+        showingPriorityPopover = false
+    }
+    
+    func setPriorityLow() {
+        priority = .low
+        showingPriorityPopover = false
     }
 }
